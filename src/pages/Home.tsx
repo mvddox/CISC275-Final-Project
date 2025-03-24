e81e97f266607663fb66e0b2b162584c8f937138
Elijah Jeudy <elijahj@udel.edu>
2025-03-24 14:08:06 -0400

    Updating this branch to match main

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -40,9 +40,6 @@ function HomePage() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload. 
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
