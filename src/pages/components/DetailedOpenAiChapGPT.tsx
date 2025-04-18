6b8166c51bdf6203218fc173a3d9b6ec0271031f
Ethan Rigor <ethanrig@udel.edu>
2025-04-18 13:41:14 -0400

    formated responses in JSON; cleaned up final sentencing

--- src/pages/components/DetailedOpenAiChapGPT.tsx
+++ src/pages/components/DetailedOpenAiChapGPT.tsx
@@ -13,6 +13,8 @@ function OpenAiComponent({DetailedResults}:
     const [results, setResults] = useState<string[]>([]) // collection of all the results
     const [finalResult, setFinalResult] = useState<string>("") // used for final analysis
     const [finalSentence, setFinalSentence] = useState<string>("") // used for their final sentence
+    const [finalDeclaredFuture, setFinalDeclaredFuture] = useState<string>("") // final phasse
+    const [finalCareer, setFinalCareer] = useState<string>("") // final career
     const [progressMessage, setProgressMessage] = useState<string>("")
     const openai = new OpenAI({apiKey: keyData, dangerouslyAllowBrowser: true}) // because the user inputs in,
 
@@ -59,56 +61,67 @@ function OpenAiComponent({DetailedResults}:
             }
         )
         setResults([...await Promise.all(userResponses)]) 
-        
-        let finalResult:    Promise<string> = new Promise<string>(() => {
-        });
         try{
             
             await Promise.all(userResponses).then(async (userResponses)=>    
                 {
-                setProgressMessage("Judging character")
+                setProgressMessage("Arbitrating your final judgment")
                 console.log(userResponses)
                 const response = await openai.responses.create({
                 model: "gpt-4o",
                 instructions: "use second tense",
                 input: [
                     {   role: "developer",
-                        content: "The questions are: " + Object.entries(DETAILED_QUESTIONS).map(([key,value]:[string,DetailedQuestionType], index)=> ""+ index +": " + value.instruction)
+                        content: "The questions are: " + DETAILED_QUESTIONS.map((value:DetailedQuestionType, index)=> ""+ index +": " + value.instruction)
                     },
                     {   role: "developer",
                         content: "The user gave answers to those questions which you determined a result based on each corresponding question; these results are:" + userResponses.map((val)=>val)
                     },
                     {   role: "developer",
-                        content: "Based on the results: How would you define the person as a whole?"
+                        content: "Based on the results: in a many sentences how would you define the person as a whole?"
+                        + "In one sentence, how would you report their future?"
+                        + "In one phrase, what is their future?"
+                        + "what is their future job?"
                     },
                 ],
-                temperature: 1.45 //1.5 and above breaks it to random characters
+                temperature: 1.40, //1.5 and above breaks it to random characters
+                text: {
+                    format: {
+                      type: "json_schema",
+                      name: "total_result_arbitration",
+                      schema: {
+                        type: "object",
+                        properties: {
+                          user_definition: { 
+                            type: "string" 
+                          },
+                          final_sentence: { 
+                            type: "string" 
+                          },
+                          future_phrase: { 
+                            type: "string", 
+                          },
+                          future_career: { 
+                            type: "string", 
+                          },
+                        },
+                        required: ["user_definition", "final_sentence", "future_phrase", "future_career"],
+                        additionalProperties: false,
+                      },
+                    }
+                  }
                 });
-                setFinalResult(response.output_text)
-                finalResult = Promise.resolve(response.output_text)
-                setProgressMessage("Arbitrating your final judgment")
+                setFinalResult(JSON.parse(response.output_text).user_definition)
+                setFinalSentence(JSON.parse(response.output_text).final_sentence)
+                setFinalDeclaredFuture(JSON.parse(response.output_text).future_phrase)
+                setFinalCareer(JSON.parse(response.output_text).future_career)
+                console.log(response.usage)
             })
         }
         catch (e){
             setAiError("It seems that there was an error.....")
             console.error(e);
             }
-        // final sentence for the ultimate arbitration of the person's future
-        try{
-            await finalResult.then(async (finalResult)=>{
-                const response = await openai.responses.create({
-                    model: "gpt-4o",
-                    instructions: "",
-                    input: [{role: "developer", content: "Based on the results' "+ finalResult + " 'in one sentence what would their future career be?"}],
-                    temperature: 1.5
-                });
-                setFinalSentence(response.output_text)
-            })
-            }
-        catch (e){
-            setAiError("It seems that there was an error.....")
-            console.error(e);
-        }
         setLoading(false)
     }
     return <div className="ai-container">
@@ -116,6 +129,9 @@ function OpenAiComponent({DetailedResults}:
         <div className="results" hidden={true}>
         {results.join(", ")}
         </div>  
+        <div className="final-career" hidden={loading || !finalResult}>
+        {finalDeclaredFuture +"—"+ finalCareer}
+        </div>
         <div className="final-result" hidden={loading || !finalResult}>
         {finalResult}
         </div>
@@ -126,6 +142,9 @@ function OpenAiComponent({DetailedResults}:
         <div><Button className="ai-button" onClick={startAi}>
         Generate Response
         </Button></div>
+        <div className="ai-disclaimer" hidden={loading || !finalSentence}>
+        Generated by AI: All choices are yours, and the future is ultimately in your hands. Not responsible for any user damages.
+        </div>
     </div>
 }
 export default OpenAiComponent
