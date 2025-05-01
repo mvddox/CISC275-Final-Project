c820d1eff235856144d19923676f6ca5dcc53993
mvddox <maddox@udel.edu>
2025-04-30 21:50:49 -0400

    Reset Password Function Works

--- src/pages/MyProfilePage.tsx
+++ src/pages/MyProfilePage.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { Button, Form } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
+import { Account } from "./LoginPage";
 import "./MyProfilePage.css";
 
 export let keyData = "";
@@ -12,10 +13,12 @@ if (prevKey !== null) {
 
 function MyProfilePage() {
   const [key, setKey] = useState<string>(keyData);
-  const [about, setAbout] = useState<string>("");
-  const [currentUsername, setCurrentUsername] = useState<string>("");
-  const [currentPassword, setCurrentPassword] = useState<string>("");
-  const [newPassword, setNewPassword] = useState<string>("");
+  const [about, setAbout] = useState<string>(""); // the about section for the user
+  const [currentUsername, setCurrentUsername] = useState<string>(""); // the current username
+  const [currentPassword, setCurrentPassword] = useState<string>(""); // the current user's password
+  const [newPassword, setNewPassword] = useState<string>(""); // the new password that the user wants to set
+  const [changePassFail, setChangePassFail] = useState<boolean>(false); // true if setting password failed
+  const [changePassSuccess, setChangePassSuccess] = useState<boolean>(false); // true if setting password was successful
 
   // Allowed usernames for which the "About you" section will be shown
   //const allowedUsernames = ["elijah", "ethan"]; // Replace with actual allowed usernames
@@ -70,6 +73,20 @@ function MyProfilePage() {
   function handleKeySubmit() {
     localStorage.setItem(saveKeyData, JSON.stringify(key));
     window.location.reload();
+  }
+  
+  //resets password if given current password is valid
+  function handleResetPassword(){
+    let account:Account = JSON.parse(localStorage.getItem(currentUsername) || "");
+    if (account.password === currentPassword){
+      account.password = newPassword;
+      localStorage.setItem(currentUsername, JSON.stringify(account));
+      setChangePassFail(false);
+      setChangePassSuccess(true);
+      return;
+    }
+    setChangePassFail(true);
+    setChangePassSuccess(false);
   }
 
   // Save the "About you" section to localStorage
@@ -143,6 +160,15 @@ function MyProfilePage() {
                   onChange={updateNewPassword} />
           </Form.Group>
         </Form>
+          <div className = 'resetFailMessage'>
+            {(changePassFail && "Current Password Does Not Match")}
+          </div>
+          <div className = 'resetSuccessMessage'>
+            {(changePassSuccess && "Password Successfully Reset")}
+          </div>
+          <div className = 'passwordButton'>
+            <Button onClick = {handleResetPassword}>Reset</Button>
+          </div>
         </div>
         {/* "About You" Section that is always visible */}
         <Form onSubmit={handleProfileSubmit}>
